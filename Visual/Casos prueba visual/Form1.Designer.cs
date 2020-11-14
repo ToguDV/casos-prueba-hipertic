67f09863cf893ca40e4ef6c2dc7a8689193d17c2
JuanDCorreaZ <65989626+JuanDCorreaZ@users.noreply.github.com>
2020-11-14 01:02:58 -0500

    Agrego los 14 botones terminados

--- Visual/Casos prueba visual/Form1.Designer.cs
+++ Visual/Casos prueba visual/Form1.Designer.cs
@@ -35,6 +35,14 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -50,65 +58,157 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(152, 167);
+            this.button1.Location = new System.Drawing.Point(156, 140);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "CP001";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(335, 167);
+            this.button2.Location = new System.Drawing.Point(339, 140);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "CP002";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(515, 167);
+            this.button3.Location = new System.Drawing.Point(519, 140);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "CP003";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(152, 217);
+            this.button4.Location = new System.Drawing.Point(156, 190);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
+            this.button4.Text = "CP004";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(335, 217);
+            this.button5.Location = new System.Drawing.Point(339, 190);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 5;
-            this.button5.Text = "button5";
+            this.button5.Text = "CP005";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(515, 217);
+            this.button6.Location = new System.Drawing.Point(519, 190);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 6;
-            this.button6.Text = "button6";
+            this.button6.Text = "CP006";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(519, 235);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "CP009";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(339, 235);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "CP008";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(156, 235);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "CP007";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(519, 278);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "CP012";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(339, 278);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "CP011";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(156, 278);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 10;
+            this.button12.Text = "CP010";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(339, 323);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 14;
+            this.button13.Text = "CP014";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(156, 323);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "CP013";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -133,6 +233,14 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
     }
 }
 
--- Visual/Casos prueba visual/Form1.cs
+++ Visual/Casos prueba visual/Form1.cs
@@ -126,5 +126,329 @@ namespace Casos_prueba_visual
             process.Close();
            */
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "cd C:/Users/Usuario/Documents/GitHub/casos-prueba-hipertic/registro/CP003 && robot CP003.robot");
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = false;
+            processInfo.RedirectStandardOutput = false;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+            /* Process proc = null;
+            string targetDir = string.Format(@"D:\myd");   //this is where mybatch.bat lies
+            proc = new Process();
+            proc.StartInfo.WorkingDirectory = targetDir;
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = string.Format("cd C:\Users\cesar\PycharmProjects\prueba2\TestCases\casos-prueba-hipertic\registro\CP001 && robot CP001.robot");  //this is argument
+            proc.StartInfo.CreateNoWindow = false;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;  //this is for hiding the cmd window...so execution will happen in back ground.
+            proc.Start();
+            proc.WaitForExit(); 
+
+
+           /* var processInfo = new ProcessStartInfo("cmd.exe", "cd" + "C:/Users/cesar/PycharmProjects/prueba2/TestCases/casos-prueba-hipertic/registro/CP001");
+       //     processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            processInfo.e  //     processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+
+            process.WaitForExit();
+
+            Console.WriteLine("ExitCode: {0}", process.ExitCode);
+            process.Close();
+           */
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "cd C:/Users/Usuario/Documents/GitHub/casos-prueba-hipertic/registro/CP004 && robot CP004.robot");
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = false;
+            processInfo.RedirectStandardOutput = false;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+            /* Process proc = null;
+            string targetDir = string.Format(@"D:\myd");   //this is where mybatch.bat lies
+            proc = new Process();
+            proc.StartInfo.WorkingDirectory = targetDir;
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = string.Format("cd C:\Users\cesar\PycharmProjects\prueba2\TestCases\casos-prueba-hipertic\registro\CP001 && robot CP001.robot");  //this is argument
+            proc.StartInfo.CreateNoWindow = false;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;  //this is for hiding the cmd window...so execution will happen in back ground.
+            proc.Start();
+            proc.WaitForExit(); 
+
+
+           /* var processInfo = new ProcessStartInfo("cmd.exe", "cd" + "C:/Users/cesar/PycharmProjects/prueba2/TestCases/casos-prueba-hipertic/registro/CP001");
+       //     processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            processInfo.e  //     processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+
+            process.WaitForExit();
+
+            Console.WriteLine("ExitCode: {0}", process.ExitCode);
+            process.Close();
+           */
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "cd C:/Users/Usuario/Documents/GitHub/casos-prueba-hipertic/registro/CP005 && robot CP005.robot");
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = false;
+            processInfo.RedirectStandardOutput = false;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+            /* Process proc = null;
+            string targetDir = string.Format(@"D:\myd");   //this is where mybatch.bat lies
+            proc = new Process();
+            proc.StartInfo.WorkingDirectory = targetDir;
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = string.Format("cd C:\Users\cesar\PycharmProjects\prueba2\TestCases\casos-prueba-hipertic\registro\CP001 && robot CP001.robot");  //this is argument
+            proc.StartInfo.CreateNoWindow = false;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;  //this is for hiding the cmd window...so execution will happen in back ground.
+            proc.Start();
+            proc.WaitForExit(); 
+
+
+           /* var processInfo = new ProcessStartInfo("cmd.exe", "cd" + "C:/Users/cesar/PycharmProjects/prueba2/TestCases/casos-prueba-hipertic/registro/CP001");
+       //     processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            processInfo.e  //     processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+
+            process.WaitForExit();
+
+            Console.WriteLine("ExitCode: {0}", process.ExitCode);
+            process.Close();
+           */
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "cd C:/Users/Usuario/Documents/GitHub/casos-prueba-hipertic/registro/CP006 && robot CP006.robot");
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = false;
+            processInfo.RedirectStandardOutput = false;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+            /* Process proc = null;
+            string targetDir = string.Format(@"D:\myd");   //this is where mybatch.bat lies
+            proc = new Process();
+            proc.StartInfo.WorkingDirectory = targetDir;
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = string.Format("cd C:\Users\cesar\PycharmProjects\prueba2\TestCases\casos-prueba-hipertic\registro\CP001 && robot CP001.robot");  //this is argument
+            proc.StartInfo.CreateNoWindow = false;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;  //this is for hiding the cmd window...so execution will happen in back ground.
+            proc.Start();
+            proc.WaitForExit(); 
+
+
+           /* var processInfo = new ProcessStartInfo("cmd.exe", "cd" + "C:/Users/cesar/PycharmProjects/prueba2/TestCases/casos-prueba-hipertic/registro/CP001");
+       //     processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            processInfo.e  //     processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+
+            process.WaitForExit();
+
+            Console.WriteLine("ExitCode: {0}", process.ExitCode);
+            process.Close();
+           */
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "cd C:/Users/Usuario/Documents/GitHub/casos-prueba-hipertic/registro/CP007&& robot CP007.robot");
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = false;
+            processInfo.RedirectStandardOutput = false;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "cd C:/Users/Usuario/Documents/GitHub/casos-prueba-hipertic/registro/CP008 && robot CP008.robot");
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = false;
+            processInfo.RedirectStandardOutput = false;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "cd C:/Users/Usuario/Documents/GitHub/casos-prueba-hipertic/Test Hipertensión/CP-009 && robot CP-009.robot");
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = false;
+            processInfo.RedirectStandardOutput = false;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "cd C:/Users/Usuario/Documents/GitHub/casos-prueba-hipertic/Test Hipertensión/CP-010 && robot CP-010.robot");
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = false;
+            processInfo.RedirectStandardOutput = false;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "cd C:/Users/Usuario/Documents/GitHub/casos-prueba-hipertic/Test Hipertensión/CP-011 && robot CP-011.robot");
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = false;
+            processInfo.RedirectStandardOutput = false;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "cd C:/Users/Usuario/Documents/GitHub/casos-prueba-hipertic/Funciones Admin/CP012 && robot CP012.robot");
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = false;
+            processInfo.RedirectStandardOutput = false;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "cd C:/Users/Usuario/Documents/GitHub/casos-prueba-hipertic/Funciones Admin/CP013 && robot CP013.robot");
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = false;
+            processInfo.RedirectStandardOutput = false;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + "cd C:/Users/Usuario/Documents/GitHub/casos-prueba-hipertic/Funciones Admin/CP0-14 && robot CP014.robot");
+            processInfo.CreateNoWindow = false;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = false;
+            processInfo.RedirectStandardOutput = false;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+        }
     }
 }
